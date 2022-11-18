e96d19b0139a54fdeb2dfae5dbe51c6925d4fe34
morimoto <au19950927@gmail.com>
2022-11-18 20:09:31 +0900

    development.rbの修正

--- vendor/bundle/ruby/2.6.0/gems/nested_form-0.3.2/spec/dummy/config/environments/development.rb
+++ vendor/bundle/ruby/2.6.0/gems/nested_form-0.3.2/spec/dummy/config/environments/development.rb
@@ -12,7 +12,7 @@ Dummy::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
+  config.hosts.clear
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
